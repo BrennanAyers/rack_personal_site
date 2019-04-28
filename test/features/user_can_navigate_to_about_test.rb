58267bdb6d4120a66ef72b3f62bff065b3161ec1
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-28 12:46:21 -0600

    Change about page test to test about page

--- test/features/user_can_navigate_to_about_test.rb
+++ test/features/user_can_navigate_to_about_test.rb
@@ -1,10 +1,12 @@
 require './test/test_helper'
 
-class ErrorpageTest < CapybaraTestCase
-  def test_user_can_see_the_errorpage
-    visit '/knowhere'
+class LinkTest < CapybaraTestCase
+  def test_user_can_see_the_homepage
+    visit '/'
+    click_on "About"
 
-    assert page.has_content?("Page not found.")
-    assert_equal 404, page.status_code
+    assert_equal 200, page.status_code
+    assert_equal '/about', current_path
+    assert page.has_content?("About Me!")
   end
 end
